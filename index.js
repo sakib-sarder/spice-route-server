73026613244e4c36ac66d6521ff528c673bb3210
sakib <s.sarder232002@gmail.com>
2023-05-02 01:21:58 +0600

    chefs json file added

--- index.js
+++ index.js
@@ -2,10 +2,15 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const port = 5000;
+const chefs = require('./Data/chefs.json')
 app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("Hello From My Restaurant");
+})
+
+app.get("/chefs", (req, res) => {
+    res.send(chefs);
 })
 
 app.listen(port, () => {
