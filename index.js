51cf766b2ed2c91860adafe71e274444a121f329
sakib <s.sarder232002@gmail.com>
2023-05-01 22:39:53 +0600

    nodemon and cors added

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const port = 5000;
+app.use(cors());
+
 app.get("/", (req, res) => {
     res.send("Hello From My Restaurant");
 })
