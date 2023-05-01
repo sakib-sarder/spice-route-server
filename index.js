9e76faadb06498ac29409ad5ee41e82f8cb99a21
sakib <s.sarder232002@gmail.com>
2023-05-01 22:03:56 +0600

    index.js file created and add basic set up

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 const express = require('express');
+const app = express();
+const port = 5000;
+app.get("/", (req, res) => {
+    res.send("Hello From My Restaurant");
+})
+
+app.listen(port, () => {
+    console.log(`My Server is Running on Port: ${port}`);
+})
