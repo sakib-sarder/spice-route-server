846a8a6c30ba1d31f91d6a22941b8f6a8cee5fbf
sakib <s.sarder232002@gmail.com>
2023-05-04 05:06:15 +0600

    article data added , photo data updated, chefs data updated

--- index.js
+++ index.js
@@ -1,29 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = 5000;
-const chefs = require('./Data/chefs.json');
-const images = require('./Data/photo.json')
+const chefs = require("./Data/chefs.json");
+const images = require("./Data/photo.json");
+const articles = require("./Data/article.json");
 app.use(cors());
 
 app.get("/", (req, res) => {
-    res.send("Hello From My Restaurant");
-})
+  res.send("Hello From Spice Store");
+});
 
 app.get("/chefs", (req, res) => {
-    res.send(chefs);
-})
+  res.send(chefs);
+});
 
 app.get("/chefs/:id", (req, res) => {
-    const id = req.params.id;
-    const singleChef = chefs.find(c => c.id == id);
-    res.send(singleChef);
-})
+  const id = req.params.id;
+  const singleChef = chefs.find((c) => c.id == id);
+  res.send(singleChef);
+});
 
 app.get("/images", (req, res) => {
-    res.send(images);
+  res.send(images);
+});
+
+app.get("/articles", (req, res) => {
+    res.send(articles);
 })
 
 app.listen(port, () => {
-    console.log(`My Server is Running on Port: ${port}`);
-})
+  console.log(`My Server is Running on Port: ${port}`);
+});
